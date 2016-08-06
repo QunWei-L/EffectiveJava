87f9ba4a3c0b4c95149d5f5b937fdd869af834e8
QunWei <lin919980674@gmail.com>
2016-08-06 15:16:59 +0800

    add law 6 , 7

---
+++ src/main/java/me/qunwei/chapter1/Law6_CancelOldRef/Stack.java
@@ -0,0 +1,41 @@
+package me.qunwei.chapter1.Law6_CancelOldRef;
+
+/**
+ * Created by QunWei on 2016/8/6.
+ */
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+/**
+ * 及时清理过期引用, 防止积聚, 内存泄漏.
+ */
+public class Stack {
+    private Object[] nodes;
+    private int key = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16; //默认初始容量
+
+    public Stack() {
+        nodes = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object node) {
+        ensureCapacity();
+        nodes[key++] = node;
+    }
+
+    public Object pop(){
+        if (key == 0){
+            throw new EmptyStackException();
+        }
+        return nodes[--key];
+    }
+
+    private void ensureCapacity() {
+        if (nodes.length == key) {
+            //TODO: what's wrong with Arrays.copyOf().
+//            nodes = Arrays.copyOf(nodes, 2 * key + 1);
+        }
+    }
+
+}
---
+++ src/main/java/me/qunwei/chapter1/Law7_NotFinalizer/Demo.java
@@ -0,0 +1,41 @@
+package me.qunwei.chapter1.Law7_NotFinalizer;
+
+/**
+ * Created by QunWei on 2016/8/6.
+ */
+
+
+/**
+ *  避免使用终结方法(finalizer), 因为不可预测/危险.
+ */
+public class Demo {
+    public Demo() {
+    }
+
+    //Do not use this method!!!
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    public void deal(){
+
+    }
+
+    public void terminate(){
+        //Destroy itself.
+    }
+
+    public static void main(String[] args) {
+
+        Demo demo = new Demo();
+
+        try {
+            demo.deal();
+        }finally {
+            demo.terminate();//手动调用总结方法
+        }
+
+    }
+
+}
