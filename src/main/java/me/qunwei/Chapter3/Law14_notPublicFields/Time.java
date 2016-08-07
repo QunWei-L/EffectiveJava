25a3659bf79dbb60d332704a8e5f2d03e594ce89
QunWei <lin919980674@gmail.com>
2016-08-07 16:39:33 +0800

    add law 13 ,14 ,15 ,16

---
+++ src/main/java/me/qunwei/Chapter3/Law14_notPublicFields/Time.java
@@ -0,0 +1,47 @@
+package me.qunwei.Chapter3.Law14_notPublicFields;
+
+/**
+ * Created by QunWei on 2016/8/7.
+ */
+
+import java.util.TimerTask;
+
+/**
+ * 公有类永远都不应该暴露 可变 的域(成员变量), 使用访问方法(Getter Setter)
+ */
+public class Time {
+    private static final int HOURS_PER_DAY = 24;
+    private static final int MINUTES_PER_HOUR = 60;
+
+    public final int hour;
+    public final int minute;
+
+    private String Country;
+    private int areaCode;
+
+    public Time(int hour, int minute) {
+        if (hour < 0 || hour > HOURS_PER_DAY)
+            throw new IllegalArgumentException("Hour:" + hour);
+
+        if (minute < 0 || minute > MINUTES_PER_HOUR)
+            throw new IllegalArgumentException("Minute:" + minute);
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public void setAreaCode(int areaCode) {
+        this.areaCode = areaCode;
+    }
+}
